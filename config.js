51d56ef8959c0af00cb01c93eff3c99d8ab6a407
Floppy <nastyakogalim@gmail.com>
2015-08-20 21:02:11 +0300

    adding all the files

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	"database": "mongodb://root:12345@ds039550.mongolab.com:39550/userstory", //MongoDB
+	"port": process.env.Port || 3000,
+	"secretKey": "YourSecretKey"
+}
---
+++ public/app/app.js
@@ -0,0 +1,6 @@
+angular.module('MyApp', ['appRoutes', 'mainCtrl', 'authService', 'userCtrl', 'userService', 'storyService', 'storyCtrl'])
+
+.config(function($httpProvider) {
+
+	$httpProvider.interceptors.push('AuthInterceptor');
+});
